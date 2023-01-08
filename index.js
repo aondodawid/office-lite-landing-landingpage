93dbd0f5f99487c27e9cea0ca44866487b7cb06a
aondodawid <aondo1@gmail.com>
2023-01-08 23:19:57 +0100

    Repair of rendering display message text

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import timer from "./timer.js";
 import createCustomSelect from "./custom-select.js";
 
-timer();
-createCustomSelect();
-
 const SignUpPage = document.getElementById("signUp");
+const DataType = document.querySelectorAll("[data-type]");
 const emailContainer = document.getElementById("emailContainer");
 const emailInput = document.getElementById("emailInput");
 const email = document.querySelector(".email");
 const form = document.querySelector("form");
-const name = document.getElementById("name");
-// const form = document.getElementById('send-data')
+
+timer();
+createCustomSelect();
 
 emailInput.addEventListener("invalid", (event) => {
   event.preventDefault();
@@ -23,24 +22,34 @@ emailInput.addEventListener("focus", () => {
   emailContainer.classList.remove("error-container");
 });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  SignUpPage.style.background = "#fff";
+function renderText() {
+  let inpuTextHtml = "";
+  DataType.forEach((data, index) => {
+    inpuTextHtml += `
+      <p style="text-align: left"class="article__paragraph--aftersignup"><b>${data.dataset.type}:</b> 
+      ${document.forms["send-data"].elements[index].value} </p>
+    `;
+  });
+
   SignUpPage.innerHTML = `
-    <header class="header--signup">
+    <header class="header--signup" style="text-align: center"  >
       <img src="./assets/shared/logo.svg" alt="logo" />
     </header>
-    <div  style="text-align: center; font-size: 1.5rem">
+    <div  style="text-align: center; font-size: 1.5rem; padding: 0 2rem">
       <h1 style="font-size:2.5rem;margin-bottom:1rem">Thank's for join to our community!</h1>
-      <p class="aricle__paragraph--aftersignup">
+      <p style="margin-bottom:1.5rem"class="article__paragraph--aftersignup">
         We are contact with You as soon as it is possible
         your sign up data is:
       </p>
-      <p class="aricle__paragraph--aftersignup "><b>Name:</b> ${name.value} </p>
-      <p class="aricle__paragraph--aftersignup "><b>Email:</b> ${emailInput.value} </p>
-      <p class="aricle__paragraph--aftersignup "><b>Your program:</b> ${document.forms["send-data"].elements[2].value} </p>
-      <p class="aricle__paragraph--aftersignup "><b>Your program:</b> ${document.forms["send-data"].elements[3].value} </p>
-      <p class="aricle__paragraph--aftersignup "><b>Your program:</b> ${document.forms["send-data"].elements[4].value} </p>
+      <div class="paragraph-container" id="paragraph-container">
+      ${inpuTextHtml}
+      </div>
     </div>
   `;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  SignUpPage.style.background = "#fff";
+  renderText();
 });
